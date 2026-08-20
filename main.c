ab316d3f49a3823ca913ae51c9a3876f4c190633
Evily09 <elaviny031@gmail.com>
2026-08-20 12:51:41 -0300

    chore: estrutura inicial do projeto

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char nome[50];
+    int idade;
+    float nota;
+
+    printf("=== CADASTRO DE ALUNO ===\n");
+
+    printf("Digite o nome do aluno: ");
+    scanf(" %[^\n]", nome);
+
+    printf("Digite a idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite a nota: ");
+    scanf("%f", &nota);
+
+    printf("\n=== DADOS DO ALUNO ===\n");
+    printf("Nome: %s\n", nome);
+    printf("Idade: %d anos\n", idade);
+    printf("Nota: %.1f\n", nota);
+
+    return 0;
+}
